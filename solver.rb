9c14ec452a7c98624f60bb140391f048da8627ed
Jared Grippe <jared@devbootcamp.com>
2015-07-17 17:43:15 -0700

    WIP

--- solver.rb
+++ solver.rb
@@ -1,26 +1,54 @@
 class Solver
 
-  Cell = Struct.new(:x, :y, :mirror)
+  Cell = Struct.new(:board, :x, :y) do
+    def mirror
+      board.mirror(x, y)
+    end
+  end
 
-  Position = Struct.new(:x, :y, :direction) do
+  Position = Struct.new(:board, :cell, :direction) do
+    def x; cell.x; end
+    def y; cell.y; end
     def next
       next_x, next_y = case direction
-        when :N; [x, y-1]
-        when :S; [x, y+1]
-        when :E; [x+1, y]
-        when :W; [x-1, y]
+        when :N; [cell.x,   cell.y-1]
+        when :S; [cell.x,   cell.y+1]
+        when :E; [cell.x+1, cell.y  ]
+        when :W; [cell.x-1, cell.y  ]
+        else; binding.pry
       end
-      next_direction = 
-      Position.new(next_x, next_y, next_direction)
+
+      next_cell = board.cell(next_x, next_y)
+
+      next_direction = case [next_cell.mirror, direction]
+        when [nil,  :N]; :N
+        when [nil,  :S]; :S
+        when [nil,  :E]; :E
+        when [nil,  :W]; :W
+        when [:'/', :N]; :E
+        when [:'/', :S]; :W
+        when [:'/', :E]; :N
+        when [:'/', :W]; :S
+        when [:'\\',:N]; :W
+        when [:'\\',:S]; :E
+        when [:'\\',:E]; :S
+        when [:'\\',:W]; :N
+        else; binding.pry
+      end
+      self.class.new(board, next_cell, next_direction)
     end
   end
 
 
   class Board
 
-    ParseError = Class.new(StandardError)
+    DIRECTIONS = [:N, :S, :E, :W].freeze
 
-    attr_reader :to_s, :width, :height, :player, :mirrors
+    InvalidCellError = Class.new(StandardError)
+    ParseError = Class.new(StandardError)
+    InvalidDirectionError = Class.new(StandardError)
+
+    attr_reader :to_s, :width, :height, :player
 
     def initialize(puzzle)
       @to_s = puzzle
@@ -29,78 +57,56 @@ class Solver
       @width, @height = $1.to_i, $2.to_i
 
       lines.shift.match(%r<^(\d+) (\d+) ([NSEW])$>) or raise ParseError.new('failed to parse player position')
-      @player = Position.new($1.to_i, $2.to_i, $3.to_sym)
+      @player = position($1.to_i, $2.to_i, $3.to_sym)
 
-      @mirrors = lines.map do |line|
+      @mirrors = {}
+      lines.each do |line|
         line.match(%r<^(\d+) (\d+) ([\/\\])$>) or raise ParseError.new('failed to parse mirror position')
-        Cell.new($1.to_i, $2.to_i, $3.to_sym)
-      end.freeze
+        @mirrors[[$1.to_i, $2.to_i]] = $3.to_sym
+      end
+      @mirrors.freeze
+    end
+
+    def mirror(x, y)
+      @mirrors[[x,y]]
+    end
+
+    def cell(x, y)
+      if x < 0 || x >= width || y < 0 || y >= height
+        raise InvalidCellError.new("#{x},#{y} is an invalid cell")
+      end
+      Cell.new(self, x, y)
+    end
+
+    def position(x, y, direction)
+      if !DIRECTIONS.include?(direction)
+        raise InvalidDirectionError, "#{direction.inspect} is not a valid direction"
+      end
+      Position.new(self, cell(x, y), direction)
     end
   end
 
-
-  # attr_reader :puzzle, :size_x, :size_y, :player_x, :player_y
-  # attr_reader :player_direction, :mirrors
-  attr_reader :board
+  attr_reader :board, :solution
 
   def initialize(puzzle)
     @board = Board.new(puzzle)
     @path = []
-    # solve!
+    solve!
   end
 
-  # private
+  private
 
-  # def solve!
-  #   @lazer_position = @player_position.dup
-  #   lazer_x, lazer_y, lazer_direction = @player_x, @player_y, @player_direction
-  #   loop do
-  #     @next_lazer_position = @lazer_position.next
-
-  #     next_path = [lazer_x, lazer_y, lazer_direction]
-  #     return '-1' if @path.include?(next_path)
-  #     @path.push next_path
-  #     # next_x, next_y = case lazer_direction
-  #     #   when 'N'; [lazer_x, lazer_y-1]
-  #     #   when 'S'; [lazer_x, lazer_y+1]
-  #     #   when 'E'; [lazer_x+1, lazer_y]
-  #     #   when 'W'; [lazer_x-1, lazer_y]
-  #     # end
-
-  #     next_cell = cell_for(next_x, next_y)
-  #     break if next_cell.nil?
-
-  #     next_direction = case [next_cell,lazer_direction]
-  #       when ['',  'N']; 'N' 
-  #       when ['',  'S']; 'S' 
-  #       when ['',  'E']; 'E' 
-  #       when ['',  'W']; 'W' 
-  #       when ['/', 'N']; 'E'
-  #       when ['/', 'S']; 'W'
-  #       when ['/', 'E']; 'N'
-  #       when ['/', 'W']; 'S'
-  #       when ['\\','N']; 'W'
-  #       when ['\\','S']; 'E'
-  #       when ['\\','E']; 'S'
-  #       when ['\\','W']; 'N'
-  #     end
-  #     lazer_x, lazer_y, lazer_direction = next_x, next_y, next_direction
-  #   end
-  #   "#{@path.length}\n#{@path.last[0]} #{@path.last[1]}"
-  # end
-
-  # def cell_for(x, y)
-  #   return nil if x > (@size_x-1) || y > (@size_y-1)
-  #   @mirrors.each do |(mx, my, mirror)|
-  #     return mirror if x == mx && y == my
-  #   end
-  #   return ""
-  # end
-
-  # def mirror?(x, y)
-  #   @mirrors.each do |(mx, my, direction)|
-  #     return direction if x == mx && y == my
-  #   end
-  # end
+  def solve!
+    @lazer_position = board.player.dup
+    loop do
+      return @solution = '-1' if @path.include? @lazer_position
+      @path << @lazer_position
+      begin
+        @lazer_position = @lazer_position.next
+      rescue Solver::Board::InvalidCellError => error
+        return @solution = "#{@path.length}\n#{@path.last.x} #{@path.last.y}"
+      end
+    end
+  end
 
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,29 +7,33 @@ describe Solver do
     it 'should parse that shit' do
       puzzle = format_puzzle %q(
         3 4
-        0 0 N
-        0 1 /
-        0 2 \
+        0 1 N
+        0 2 /
+        0 3 \
       )
       board = Solver::Board.new(puzzle)
 
-      expect( board.to_s    ).to eq puzzle
-      expect( board.width   ).to eq 3
-      expect( board.height  ).to eq 4
-      expect( board.player  ).to eq Solver::Position.new(0, 0, :N)
-      expect( board.mirrors ).to eq [
-        Solver::Cell.new(0,1,:/),
-        Solver::Cell.new(0,2,:'\\')
-      ]
+      expect( board.to_s             ).to eq puzzle
+      expect( board.width            ).to eq 3
+      expect( board.height           ).to eq 4
+      expect( board.player.x         ).to eq 0
+      expect( board.player.y         ).to eq 1
+      expect( board.player.direction ).to eq :N
+      expect( board.mirror(0,0)      ).to eq nil
+      expect( board.mirror(0,1)      ).to eq nil
+      expect( board.mirror(0,2)      ).to eq :/
+      expect( board.mirror(0,3)      ).to eq :'\\'
     end
   end
 
   it 'should take a puzzle as a string and parse it' do
     puzzle = format_puzzle %q(
       3 3
-      0 0 N
-      0 1 /
+      0 1 S 
+      0 0 /
       0 2 \
+      2 0 \
+      2 2 /
     )
 
     solver = Solver.new(puzzle)
@@ -38,12 +42,12 @@ describe Solver do
     expect( solver.board.width            ).to eq 3
     expect( solver.board.height           ).to eq 3
     expect( solver.board.player.x         ).to eq 0
-    expect( solver.board.player.y         ).to eq 0
-    expect( solver.board.player.direction ).to eq :N
-    expect( solver.board.mirrors          ).to eq [
-      Solver::Cell.new(0,1,:/),
-      Solver::Cell.new(0,2,:'\\')
-    ]
+    expect( solver.board.player.y         ).to eq 1
+    expect( solver.board.player.direction ).to eq :S
+    expect( solver.board.mirror(0,0)      ).to eq :/
+    expect( solver.board.mirror(0,2)      ).to eq :'\\'
+    expect( solver.board.mirror(2,0)      ).to eq :'\\'
+    expect( solver.board.mirror(2,2)      ).to eq :/
   end
 
 
